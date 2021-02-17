59c0ee1aa6cbcc6e97431bbfe95b67ba2da657be
Luteya Coulston <thatcoul@gmail.com>
2021-02-17 13:57:54 +0300

    Updated store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import lyrics from "./modules/lyrics";
+import filters from "./modules/filters";
 
 const store = createStore({
   modules: {
     lyrics,
+    filters,
   },
   plugins: [createPersistedState()],
 });
